4baf5f48473649500a28b9f218c604705154db8a
matthieudelmont <matthieudelmont@spectre-biotech.com>
2025-12-04 13:40:40 +0100

    update history card

--- config/histoire.js
+++ config/histoire.js
@@ -1,148 +1,144 @@
 const histoireCards = [
-  {
-    "id": 37,
-    "category": "histoire",
-    "title": "L'Ancrage",
-    "typeCode": "H",
-    "rectoFile": "H_R_Ancrage_155.svg",
-    "versoFile": "H_V_Ancrage_155.svg",
-    "number": 155
-  },
-  {
-    "id": 38,
-    "category": "histoire",
-    "title": "L'appel Orion",
-    "typeCode": "H",
-    "rectoFile": "H_R_Appel-Orion_1.svg",
-    "versoFile": "H_V_Appel-orion_1.svg",
-    "number": 1
-  },
-  {
-    "id": 39,
-    "category": "histoire",
-    "title": "L'écho retrouvé",
-    "typeCode": "H",
-    "rectoFile": "H_R_Echo-Retrouve_171.svg",
-    "versoFile": "H_V_Echo-Retrouve_171.svg",
-    "number": 171
-  },
-  {
-    "id": 40,
-    "category": "histoire",
-    "title": "L'éveil des cordes",
-    "typeCode": "H",
-    "rectoFile": "H_R_Eveil-Des-Cordes_103.svg",
-    "versoFile": "H_V_Eveil-Des-Cordes_103.svg",
-    "number": 103
-  },
-  {
-    "id": 41,
-    "category": "histoire",
-    "title": "Harmonie dispersée",
-    "typeCode": "H",
-    "rectoFile": "H_R_Harmonie-dispersee_198.svg",
-    "versoFile": "H_V_Harmonie-dispersee_198.svg",
-    "number": 198
-  },
-  {
-    "id": 42,
-    "category": "histoire",
-    "title": "La chromharmonie",
-    "typeCode": "H",
-    "rectoFile": "H_R_La-Chromharmonie_716.svg",
-    "versoFile": "H_V_La-Chromharmonie_716.svg",
-    "number": 716
-  },
-  {
-    "id": 43,
-    "category": "histoire",
-    "title": "Le dernier fragment",
-    "typeCode": "H",
-    "rectoFile": "H_R_La-Le-Dernier-Fragment_485.svg",
-    "versoFile": "H_V_La-Le-Dernier-Fragment_485.svg",
-    "number": 485
-  },
-  {
-    "id": 44,
-    "category": "histoire",
-    "title": "Le dessin intérieur",
-    "typeCode": "H",
-    "rectoFile": "H_R_La-Le-Dessin-Interieur_706.svg",
-    "versoFile": "H_V_La-Le-Dessin-Interieur_706.svg",
-    "number": 706
-  },
-  {
-    "id": 45,
-    "category": "histoire",
-    "title": "Le port de Zubenelgenubi",
-    "typeCode": "H",
-    "rectoFile": "H_R_La-Le-Port-de-Zubenelgenubi_380.svg",
-    "versoFile": "H_V_La-Le-Port-de-Zubenelgenubi_380.svg",
-    "number": 380
-  },
-  {
-    "id": 46,
-    "category": "histoire",
-    "title": "La rivière qui murmure",
-    "typeCode": "H",
-    "rectoFile": "H_R_La-Riviere-qui-Murmure_365.svg",
-    "versoFile": "H_V_La-Riviere-qui-Murmure_365.svg",
-    "number": 365
-  },
-  {
-    "id": 47,
-    "category": "histoire",
-    "title": "Le chant retrouvé",
-    "typeCode": "H",
-    "rectoFile": "H_R_Le-Chant-Retrouver_273.svg",
-    "versoFile": "H_V_Le-Chant-Retrouver_273.svg",
-    "number": 273
-  },
-  {
-    "id": 48,
-    "category": "histoire",
-    "title": "Libération",
-    "typeCode": "H",
-    "rectoFile": "H_R_Le-Liberation_41.svg",
-    "versoFile": "H_V_Le-Liberation_41.svg",
-    "number": 41
-  },
-  {
-    "id": 49,
-    "category": "histoire",
-    "title": "Le silence se tend",
-    "typeCode": "H",
-    "rectoFile": "H_R_Le-Silence-Se-Tends_253.svg",
-    "versoFile": "H_V_Le-Silence-Se-Tends_253.svg",
-    "number": 253
-  },
-  {
-    "id": 50,
-    "category": "histoire",
-    "title": "Les cordes retrouvées",
-    "typeCode": "H",
-    "rectoFile": "H_R_Les-Cordes-Retrouvées_285.svg",
-    "versoFile": "H_V_Les-Cordes-Retrouvées_285.svg",
-    "number": 285
-  },
-  {
-    "id": 51,
-    "category": "histoire",
-    "title": "L'oubli du sagittaire",
-    "typeCode": "H",
-    "rectoFile": "H_R_Oublie-Du-Sagittaire_518.svg",
-    "versoFile": "H_V_Oublie-Du-Sagittaire_518.svg",
-    "number": 518
-  },
-  {
-    "id": 52,
-    "category": "histoire",
-    "title": "Recontre avec le lapin",
-    "typeCode": "H",
-    "rectoFile": "H_R_Recontre-avec-le-Lapin_258.svg",
-    "versoFile": "H_V_Recontre-avec-le-Lapin_258.svg",
-    "number": 258
-  }
+    {
+        id: 'histoire-1',
+        title: 'Appel Orion',
+        number: '1',
+        recto: 'images/cartes/histoires/H_R_Appel-Orion_1.svg',
+        verso: 'images/cartes/histoires/H_V_Appel-orion_1.svg'
+    },
+    {
+        id: 'histoire-48',
+        title: 'La complétion du trou',
+        number: '48',
+        recto: 'images/cartes/histoires/H_R_La-completion-du-trou_48.svg',
+        verso: 'images/cartes/histoires/H_V_La-complétion-du-trou_48.svg'
+    },
+    {
+        id: 'histoire-90',
+        title: 'Libération',
+        number: '90',
+        recto: 'images/cartes/histoires/H_R_Liberation_90.svg',
+        verso: 'images/cartes/histoires/H_V_Liberation_90.svg'
+    },
+    {
+        id: 'histoire-103',
+        title: 'Éveil Des Cordes',
+        number: '103',
+        recto: 'images/cartes/histoires/H_R_Eveil-Des-Cordes_103.svg',
+        verso: 'images/cartes/histoires/H_V_Eveil-Des-Cordes_103.svg'
+    },
+    {
+        id: 'histoire-128',
+        title: 'Le Silence Se Tend',
+        number: '128',
+        recto: 'images/cartes/histoires/H_R_Le-Silence-Se-Tend_128.svg',
+        verso: 'images/cartes/histoires/H_V_Le-Silence-Se-Tend_128.svg'
+    },
+    {
+        id: 'histoire-155',
+        title: 'Ancrage',
+        number: '155',
+        recto: 'images/cartes/histoires/H_R_Ancrage_155.svg',
+        verso: 'images/cartes/histoires/H_V_Ancrage_155.svg'
+    },
+    {
+        id: 'histoire-171',
+        title: 'Écho Retrouvé',
+        number: '171',
+        recto: 'images/cartes/histoires/H_R_Echo-Retrouve_171.svg',
+        verso: 'images/cartes/histoires/H_V_Echo-Retrouve_171.svg'
+    },
+    {
+        id: 'histoire-198',
+        title: 'Harmonie dispersée',
+        number: '198',
+        recto: 'images/cartes/histoires/H_R_Harmonie-dispersee_198.svg',
+        verso: 'images/cartes/histoires/H_V_Harmonie-dispersee_198.svg'
+    },
+    {
+        id: 'histoire-253',
+        title: 'Le geste restitué',
+        number: '253',
+        recto: 'images/cartes/histoires/H_R_le-geste-restitue_253.svg',
+        verso: 'images/cartes/histoires/H_V_le-geste-restitue_253.svg'
+    },
+    {
+        id: 'histoire-258',
+        title: 'Rencontre avec le Lapin',
+        number: '258',
+        recto: 'images/cartes/histoires/H_R_Rencontre-avec-le-Lapin_258.svg',
+        verso: 'images/cartes/histoires/H_V_Rencontre-avec-le-Lapin_258.svg'
+    },
+    {
+        id: 'histoire-273',
+        title: 'Le Chant Retrouvé',
+        number: '273',
+        recto: 'images/cartes/histoires/H_R_Le-Chant-Retrouver_273.svg',
+        verso: 'images/cartes/histoires/H_V_Le-Chant-Retrouver_273.svg'
+    },
+    {
+        id: 'histoire-285',
+        title: 'Les Cordes Retrouvées',
+        number: '285',
+        recto: 'images/cartes/histoires/H_R_Les-Cordes-Retrouvées_285.svg',
+        verso: 'images/cartes/histoires/H_V_Les-Cordes-Retrouvées_285.svg'
+    },
+    {
+        id: 'histoire-365',
+        title: 'La Rivière qui Murmure',
+        number: '365',
+        recto: 'images/cartes/histoires/H_R_La-Riviere-qui-Murmure_365.svg',
+        verso: 'images/cartes/histoires/H_V_La-Riviere-qui-Murmure_365.svg'
+    },
+    {
+        id: 'histoire-380',
+        title: 'Le Port de Zubenelgenubi',
+        number: '380',
+        recto: 'images/cartes/histoires/H_R_La-Le-Port-de-Zubenelgenubi_380.svg',
+        verso: 'images/cartes/histoires/H_V_La-Le-Port-de-Zubenelgenubi_380.svg'
+    },
+    {
+        id: 'histoire-387',
+        title: 'Le Mauvais Fragment',
+        number: '387',
+        recto: 'images/cartes/histoires/H_R_La-Le-Mauvais-Fragment_387.svg',
+        verso: 'images/cartes/histoires/H_V_La-Le-Mauvais-Fragment_387.svg'
+    },
+    {
+        id: 'histoire-485',
+        title: 'Le Dernier Fragment',
+        number: '485',
+        recto: 'images/cartes/histoires/H_R_La-Le-Dernier-Fragment_485.svg',
+        verso: 'images/cartes/histoires/H_V_La-Le-Dernier-Fragment_485.svg'
+    },
+    {
+        id: 'histoire-515',
+        title: 'Le sillage conservé',
+        number: '515',
+        recto: 'images/cartes/histoires/H_R_Le-sillage-conserve_515.svg',
+        verso: 'images/cartes/histoires/H_V_le-sillage-conserve_515.svg'
+    },
+    {
+        id: 'histoire-518',
+        title: 'Oublié Du Sagittaire',
+        number: '518',
+        recto: 'images/cartes/histoires/H_R_Oublie-Du-Sagittaire_518.svg',
+        verso: 'images/cartes/histoires/H_V_Oublie-Du-Sagittaire_518.svg'
+    },
+    {
+        id: 'histoire-706',
+        title: 'Le Dessin Intérieur',
+        number: '706',
+        recto: 'images/cartes/histoires/H_R_La-Le-Dessin-Interieur_706.svg',
+        verso: 'images/cartes/histoires/H_V_La-Le-Dessin-Interieur_706.svg'
+    },
+    {
+        id: 'histoire-716',
+        title: 'La Chromharmonie',
+        number: '716',
+        recto: 'images/cartes/histoires/H_R_La-Chromharmonie_716.svg',
+        verso: 'images/cartes/histoires/H_V_La-Chromharmonie_716.svg'
+    }
 ];
 
 export default histoireCards;
